2d7dba91602bd7158afeb0916acd08c95832f2bb
Lakshay-2003 <Lakshay.Saxena@esyasoft.com>
2025-09-04 17:41:06 +0530

    Day_2

---
+++ Python_basics/Day_2/module_demo.py
@@ -0,0 +1,10 @@
+import calculator
+
+result1 = calculator.sum(10, 5)
+print("Sum:", result1)
+result2 = calculator.subtract(10, 5)
+print("Subtract:", result2)
+result3 = calculator.multiply(10, 5)
+print("Multiply:", result3)
+result4 = calculator.divide(10, 0)
+print("Divide:", result4)           
