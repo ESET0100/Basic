b2481ed33c3e9ea5b33966940a050e2400f47ead
Lakshay-2003 <Lakshay.Saxena@esyasoft.com>
2025-09-15 16:11:20 +0530

    Day_3

---
+++ DotNet/Class_Tasks/Class_demo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Class_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student rohan = new student(1, "Rohan Mishra", 22, "rohan@gmail.com", "9876543234");
+            rohan.display();
+        }
+    }
+}
---
+++ DotNet/Class_Tasks/Class_demo/student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_demo
+{
+    internal class student
+    {
+        int studentId;
+        string studentName;
+        int age;
+        string email;
+        string contact;
+        
+        public void initialize()
+        {
+            studentId = 10;
+            studentName = "John";
+            age = 22;
+
+        }
+        public void display()
+        {
+            Console.WriteLine("Student id: "+ this.studentId);
+            Console.WriteLine("Student name: "+ this.studentName);
+        }
+        public student(int studentId, string studentName, int age, string email, string contact)
+        {
+            this.studentId = studentId;
+            this.studentName = studentName;
+            this.age = age;
+            this.email = email;
+            this.contact = contact;
+        }
+    }
+}
