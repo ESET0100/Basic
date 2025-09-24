2b409a880bcb02cba4a0dc79612edbf98fe14404
Lakshay-2003 <Lakshay.Saxena@esyasoft.com>
2025-09-24 17:09:48 +0530

    Day_7

---
+++ DotNet/Class_Tasks/Day_7/Program.cs
@@ -0,0 +1,18 @@
+﻿using Day_7;
+
+namespace Day_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SavingsAccount acc = new SavingsAccount("SB001", "Amit Kumar", 1000.0);
+            acc.DisplayBalance();
+            acc.Deposit(500);
+            acc.Withdraw(200);
+            acc.ApplyInterest();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DotNet/Class_Tasks/Day_7/Savings_class.cs
@@ -0,0 +1,27 @@
+﻿using Day_7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_7
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount(string accountNumber, string holderName, double balance = 0.0, double interestRate = 0.05)
+            : base(accountNumber, holderName, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void ApplyInterest()
+        {
+            double interest = Balance * InterestRate;
+            Balance += interest;
+            Console.WriteLine($"Interest of ₹{interest} applied. New Balance: ₹{Balance}");
+        }
+    }
+}
